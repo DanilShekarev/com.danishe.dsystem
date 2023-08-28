15eb162d498c8717114461d57bf228a9bbb965c7
DanilShekarev <shekarev-danil@mail.ru>
2023-08-28 12:09:29 +0400

    Interfaces

---
+++ Interfaces/IValueProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DSystem.Attributes;
+
+namespace DSystem.Interfaces
+{
+    [ProviderInterface]
+    public interface IValueProvider<T> where T : struct
+    {
+        ValueProvider<T> ValueProvider { get; }
+        
+        [Subscribe]
+        public void Subscribe(Action<T> action)
+        {
+            ValueProvider.ValueChangedEvent += action;
+            action?.Invoke(ValueProvider.Value);
+        }
+
+        [UnSubscribe]
+        public void UnSubscribe(Action<T> action)
+        {
+            ValueProvider.ValueChangedEvent -= action;
+        }
+    }
+}
